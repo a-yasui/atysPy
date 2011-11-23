4b848acd22bfbdf22019bbfc47145d9de83fa929
Atsushi Yasui <a.yasui@gmail.com>
2011-11-23 18:51:24 +0900

    Forget the chr method. not method, it's function...

--- mksecret2.py
+++ mksecret2.py
@@ -1,11 +1,11 @@
 #-*- coding: utf8 -*-
-# Performance check: https://docs.google.com/spreadsheet/ccc?key=0ArOp8Xgv8r8RdEw3enpNLWdkUDNReDM3bHduOUFkZEE
+# Performance check: https://docs.google.com/spreadsheet/ccc?key=0ArOp8Xgv8r8RdGpCd1d2ejdCbm5EYThZcUZoZkhtX3c
 
 import random
 import time
 
 def create_secret(length):
-    return "".join(["%c"%(random.randint(33,126)) for b in range(0, length)])
+    return "".join([chr(random.randint(33,126)) for b in range(0, length)])
 
 
 buff = ""
