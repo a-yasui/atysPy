d220c869e8a2f984ed87f64f1e33eadb2f6f5626
Atsushi Yasui @ MBA <a.yasui@gmail.com>
2012-10-29 15:19:13 +0900

    update option

--- mkpass.py
+++ mkpass.py
@@ -15,11 +15,18 @@ def randomstr(length):
 	return ''.join([random.choice(strings) for x in xrange(length)])
 
 if __name__ == '__main__':
-	optlist, length = getopt.getopt(sys.argv[1:], "sh")
+	optlist, length = getopt.getopt(sys.argv[1:], "shdluA")
 	
-	USAGE = r"""%(command)s [-s] [-h] length
-Options
+	USAGE = r"""%(command)s [-s] [-h] [-d] [-l] [-u] [-A] length
+  
+  Create the random string.
 
+Options::
+
+	-l: Lowercase only.
+	-u: Uppercase only.
+	-A: Lowercase, Uppercase, digit use. ``Default''
+	-d: digit only.
 	-s: Salt string. this use all shown stirng.
 	-h: show this.
 
@@ -29,8 +36,11 @@ Options
 		if opt == '-h':
 			print USAGE
 			sys.exit(0)
-		elif opt == '-s':
-			strings = "".join([chr(i) for i in range(0x21,0x7e)])
+		elif opt == '-s': strings = "".join([chr(i) for i in range(0x21,0x7e)])
+		elif opt == '-d': strings = string.digits
+		elif opt == '-u': strings = string.uppercase
+		elif opt == '-l': strings = string.lowercase
+		elif opt == '-A': strings = string.uppercase + string.lowercase + string.digits
 	
 	if not length:
 		print USAGE
